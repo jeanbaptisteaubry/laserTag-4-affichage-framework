15d39a005b50c4d337548ad3fe62dfe21bc2fc16
BTSSIO-PERGAUD <btssio@lyceepergaud.fr>
2023-01-05 16:24:55 +0100

    all

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,27 @@
- 
+
 #include <Arduino.h>
 #include "Ecran.h"
 #include "enum.h"
 
 Ecran ecran;
 
-void setup() {
-   Serial.begin(115200);
+void setup()
+{
+  Serial.begin(115200);
 
   Serial.printf("***** Démarrage *****\n");
-  
+
   ecran.init();
   ecran.effacerEcran();
-  
-}
 
-void loop() {
-  ecran.effacerEcran();
-  ecran.afficherCentrerNormal("Hello World");
+  ecran.afficherEcranJeuArmureVie(50, 50);
+  }
+
+void loop()
+{
+
+ // ecran.effacerEcran();
+  /*ecran.afficherCentrerNormal("Hello World");
   delay(2500);
   ecran.effacerEcran();
   ecran.afficherCentrerAlerte("Alerte");
@@ -26,32 +30,41 @@ void loop() {
   ecran.afficherGaucheL1("GaucheL1");
   delay(2500);
   ecran.effacerEcran();
-  ecran.afficherEcranJeu(50,50,50,modeTire::simple,etatArme::attente,0);
-delay(2500);
-  ecran.effacerEcran();
-  ecran.afficherEcranJeu(50,50,50,modeTire::rafale,etatArme::attente,0);
-delay(2500);
-  ecran.effacerEcran();
-  ecran.afficherEcranJeu(50,50,50,modeTire::automatique,etatArme::attente,0);
-delay(2500);
-  ecran.effacerEcran();
-  ecran.afficherEcranJeu(50,50,50,modeTire::rafale,etatArme::chargement,0);
-delay(2500);
-  ecran.effacerEcran();
-  ecran.afficherEcranJeu(50,50,50,modeTire::rafale,etatArme::rechargeChargeur,0);
-  delay(2500);
-   ecran.effacerEcran();
-  ecran.afficherEcranJeu(50,50,50,modeTire::rafale,etatArme::rechargeChargeur,1000);
+  */
+  /*ecran.afficherEcranJeu(50, 50, 50, modeTire::simple, etatArme::attente, 0);
   delay(2500);
   ecran.effacerEcran();
-  ecran.afficherEcranJeu(50,50,50,modeTire::rafale,etatArme::rechargeChargeur,500);
+  ecran.afficherEcranJeu(50, 50, 50, modeTire::rafale, etatArme::attente, 0);
   delay(2500);
   ecran.effacerEcran();
-  ecran.afficherEcranJeu(50,50,50,modeTire::rafale,etatArme::rechargeChargeur,50);
+  ecran.afficherEcranJeu(50, 50, 50, modeTire::automatique, etatArme::attente, 0);
   delay(2500);
   ecran.effacerEcran();
-  ecran.afficherEcranJeu(50,50,50,modeTire::rafale,etatArme::refroidissement,0);
+  ecran.afficherEcranJeu(50, 50, 50, modeTire::rafale, etatArme::chargement, 0);*/
+
+ /*
   delay(2500);
   ecran.effacerEcran();
-  ecran.afficherEcranJeu(50,50,50,modeTire::rafale,etatArme::shoot,0);
+  ecran.afficherEcranJeu(50, 50, 50, modeTire::rafale, etatArme::rechargeChargeur, 1000);
+  delay(2500);
+  ecran.effacerEcran();
+  ecran.afficherEcranJeu(50, 50, 50, modeTire::rafale, etatArme::rechargeChargeur, 500);
+   delay(2500);
+  ecran.effacerEcran();
+  ecran.afficherEcranJeu(50, 50, 50, modeTire::rafale, etatArme::rechargeChargeur, 99);
+
+  delay(2500);
+  ecran.effacerEcran();
+  ecran.afficherEcranJeu(50, 50, 50, modeTire::rafale, etatArme::rechargeChargeur, 50);
+   delay(2500);
+  ecran.effacerEcran();
+  ecran.afficherEcranJeu(50, 50, 50, modeTire::rafale, etatArme::rechargeChargeur, 0);
+
+  delay(2500);
+  ecran.effacerEcran();
+  ecran.afficherEcranJeu(50, 50, 50, modeTire::rafale, etatArme::refroidissement, 0);
+  delay(2500);
+  ecran.effacerEcran();
+  ecran.afficherEcranJeu(50, 50, 50, modeTire::rafale, etatArme::shoot, 0);
+  */
 }
