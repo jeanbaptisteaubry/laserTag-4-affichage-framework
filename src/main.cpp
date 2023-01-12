363e1866a1295c23c7b8bafe6c65b2f782fd2ba0
BTSSIO-PERGAUD <btssio@lyceepergaud.fr>
2023-01-12 09:32:36 +0100

    lycee

--- src/main.cpp
+++ src/main.cpp
@@ -21,46 +21,50 @@ int etape = 0;
 int lastTime = 0;
 void loop()
 {
-  //On fait une animation de test
+  // On fait une animation de test
   if (millis() - lastTime > 5000)
   {
     lastTime = millis();
     etape++;
     if (etape > 6)
       etape = 0;
+    Serial.printf("Etape : %d \n", etape);
   }
   switch (etape)
   {
-    case 0:
-      //Base
-      ecran.SetChangeToEcranInGame(50,50,50,modeTire::simple,etatArme::attente,0,1);
-    break;
-      
-    case 1:
-      //On tire avec une arme simple 20 shoots
-      ecran.SetChangeToEcranInGame(50,50,30,modeTire::simple,etatArme::attente,0,1);
-
-    break;
-      
-    case 2:
-      //On change le mode de tir en rafale
-      ecran.SetChangeToEcranInGame(50,50,30,modeTire::rafale,etatArme::attente,0,1);
+  case 0:
+    // Base
+    ecran.SetChangeToEcranInGame(50, 50, 50, modeTire::simple, etatArme::attente, 0, 1);
     break;
 
-    case 3:
-      //On change le mode de tir en automatique
-      ecran.SetChangeToEcranInGame(50,50,20,modeTire::automatique,etatArme::attente,0,1);
+  case 1:
+    // On tire avec une arme simple 20 shoots
+    ecran.SetChangeToEcranInGame(50, 50, 30, modeTire::simple, etatArme::attente, 0, 1);
+
     break;
 
-    case 4:
-      //On recharge le chargeur
-      ecran.SetChangeToEcranInGame(50,50,20,modeTire::automatique,etatArme::rechargeChargeur,0,1);
-    case 5:
-      //On se fait toucher dans l'armure
-      ecran.SetChangeToEcranInGame(20,50,20,modeTire::automatique,etatArme::rechargeChargeur,0,1);
-    case 6:
-      //On se fait toucher dans la vie
-      ecran.SetChangeToEcranInGame(20,20,20,modeTire::automatique,etatArme::rechargeChargeur,0,1);
+  case 2:
+    // On change le mode de tir en rafale
+    ecran.SetChangeToEcranInGame(50, 50, 30, modeTire::rafale, etatArme::attente, 0, 1);
+    break;
+
+  case 3:
+    // On change le mode de tir en automatique
+    ecran.SetChangeToEcranInGame(50, 50, 20, modeTire::automatique, etatArme::attente, 0, 1);
+    break;
+
+  case 4:
+    // On recharge le chargeur
+    ecran.SetChangeToEcranInGame(50, 50, 20, modeTire::automatique, etatArme::rechargeChargeur, 0, 1);
+    break;
+  case 5:
+    // On se fait toucher dans l'armure
+    ecran.SetChangeToEcranInGame(20, 50, 20, modeTire::automatique, etatArme::rechargeChargeur, 0, 1);
+    break;
+  case 6:
+    // On se fait toucher dans la vie
+    ecran.SetChangeToEcranInGame(20, 20, 20, modeTire::automatique, etatArme::rechargeChargeur, 0, 1);
+    break;
   }
   ecran.afficherEcranJeuMAJ();
 
